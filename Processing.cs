81aecb2af2648461702bd8f745836e60cf9620d5
godz1lla <whello24@gmail.com>
2016-03-08 13:19:31 +0300

    Added files via upload

---
+++ Processing.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroMap
+{
+    class Processing
+    {
+        public Processing(Tuple<int, int, string>[] stationList, double[,] stationsRelations)
+        {
+            StationList = stationList;
+            WeightMatrix = GetWeightMatrix(stationsRelations);
+            HistoryMatrix = GetHistoryMatrix();
+            ShortestPathMatrix = CalculateFloyd();
+        }
+
+        private readonly double Infinity = Double.PositiveInfinity;
+        private readonly Tuple<int, int, string>[] StationList;
+
+        public List<int> StationIds = new List<int>();
+
+        public double[,] HistoryMatrix;
+        public double[,] ShortestPathMatrix;
+        public double[,] WeightMatrix;
+
+
+        /// <summary>
+        /// Получение первоначальной матрицы веса исходя из состояний
+        /// </summary>
+        /// <returns></returns>
+        private double[,] GetWeightMatrix(double[,] stationsRelations)
+        {
+            var w = new double[28, 28];
+            for (int i = 0; i < w.GetLength(0); i++)
+            {
+                for (int j = 0; j < w.GetLength(0); j++)
+                {
+                    for (int k = 0; k < stationsRelations.GetLength(0); k++)
+                    {
+                        if (stationsRelations[k, 0] == i + 1 &&
+                            stationsRelations[k, 1] == j + 1)
+                        {
+                            w[j, i] = w[i, j] = stationsRelations[k, 2];
+                        }
+                        
+                    }
+                    if (w[i, j] == 0)
+                        w[i, j] = Infinity;
+                    if (w[j, i] == 0)
+                        w[j, i] = Infinity;
+
+                }
+            }
+            return w;
+        }
+
+
+        /// <summary>
+        /// Подсчет суммарной стоимости
+        /// </summary>
+        /// <returns></returns>
+        public int GetCost(List<int> stationIds)
+        {
+            int Cost = 0;
+            var stationTypesIds = from t in StationList
+                                  where stationIds.Contains(t.Item1)
+                                  select t.Item2;
+
+            if (stationTypesIds.Contains(1))
+                Cost += 1;
+            if (stationTypesIds.Contains(2))
+                Cost += 3;
+            if (stationTypesIds.Contains(3))
+                Cost += 2;
+            if (stationTypesIds.Contains(4))
+                Cost += 4;
+
+            return Cost;
+
+        }
+
+
+
+        /// <summary>
+        /// инициализация таблицы истории
+        /// </summary>
+        /// <returns></returns>
+        private double[,] GetHistoryMatrix()
+        {
+            var history = new double[28, 28];
+            for (int i = 0; i < WeightMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < WeightMatrix.GetLength(1); j++)
+                {
+                    if (WeightMatrix[i, j] == Infinity)
+                        history[i, j] = 0;
+                    else
+                        history[i, j] = i + 1;
+                }
+            }
+            return history;
+        }
+
+
+        /// <summary>
+        /// Получение идентификаторов проездных станций 
+        /// </summary>
+        public void GetStationIds(int startId, int endId)
+        {
+            var val = Convert.ToInt32(ShortestPathMatrix[endId - 1, startId - 1]);
+            if (val == endId)
+            {
+                return;
+            }
+            StationIds.Add(val);
+            GetStationIds(val, endId);
+        }
+
+
+        /// <summary>
+        /// Алгоритм Флойда
+        /// </summary>
+        private double[,] CalculateFloyd()
+        {
+            var shortestPathMatrix = HistoryMatrix;
+            for (int k = 0; k < WeightMatrix.GetLength(0); k++)
+            {
+                for (int i = 0; i < WeightMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < WeightMatrix.GetLength(0); j++)
+                    {
+                        if (WeightMatrix[i, k] != Infinity && WeightMatrix[k, j] != Infinity && (WeightMatrix[i, j] > WeightMatrix[k, j] + WeightMatrix[i, k]))
+                        {
+                            WeightMatrix[i, j] = WeightMatrix[i, k] + WeightMatrix[k, j];
+                            shortestPathMatrix[i, j] = HistoryMatrix[k, j];
+                        }
+                    }
+                }
+            }
+            return shortestPathMatrix;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroMap
+{
+    class Program
+    {
+        /// <summary>
+        /// Входные параметры для хранения в БД(номер, тип, название)
+        /// </summary>
+        private static readonly Tuple<int, int, string>[] StationNames = {
+            Tuple.Create(1, 1, "Очаковское"),
+            Tuple.Create(2, 1, "Балтика"),
+            Tuple.Create(3, 1, "Охота"),
+            Tuple.Create(4, 1, "Жигули"),
+            Tuple.Create(5, 1, "Бочка"),
+            Tuple.Create(6, 1, "Хамовники"),
+            Tuple.Create(7, 1, "Афанасий"),
+            Tuple.Create(8, 2, "Ловенбрау"),
+            Tuple.Create(9, 2, "Варштайнер"),
+            Tuple.Create(10, 2, "Гессер"),
+            Tuple.Create(11, 2, "Бакс"),
+            Tuple.Create(12, 2, "Паулайнер"),
+            Tuple.Create(13, 2, "Кромбахен"),
+            Tuple.Create(14, 2, "Амстел"),
+            Tuple.Create(15, 3, "Старопрамен"),
+            Tuple.Create(16, 3, "Крушовице"),
+            Tuple.Create(17, 3, "Козел"),
+            Tuple.Create(18, 3, "Вельвет"),
+            Tuple.Create(19, 3, "Будвайзер"),
+            Tuple.Create(20, 3, "Урквелл"),
+            Tuple.Create(21, 3, "Гамбринус"),
+            Tuple.Create(22, 4, "Хугарден"),
+            Tuple.Create(23, 4, "Бавария"),
+            Tuple.Create(24, 4, "Карлсберг"),
+            Tuple.Create(25, 4, "Леффе"),
+            Tuple.Create(26, 4, "Стелла"),
+            Tuple.Create(27, 4, "Хайникен"),
+            Tuple.Create(28, 4, "Туборг")
+        };
+
+
+        /// <summary>
+        /// Входные параметры для хранения в БД (номер станции1, номер станции2, расстояние между ними)
+        /// </summary>
+        private static readonly double[,] StationsRelations = new double[30, 3]
+        {
+        {1, 2, 7},
+        {2, 3, 8},
+        {3, 4, 2},
+        {3, 10, 4},
+        {4, 5, 1},
+        {4, 25, 3},
+        {5, 6, 3},
+        {5, 18, 1},
+        {6, 7, 4},
+        {8, 9, 5},
+        {9, 10, 7},
+        {10, 11, 2},
+        {11, 12, 4},
+        {11, 26, 5},
+        {12, 13, 3},
+        {12, 19, 3},
+        {13, 14, 9},
+        {15, 16, 7},
+        {16, 17, 3},
+        {17, 18, 4},
+        {17, 24, 4},
+        {18, 19, 3},
+        {19, 20, 3},
+        {20, 21, 8},
+        {22, 23, 5},
+        {23, 24, 4},
+        {24, 25, 5},
+        {25, 26, 3},
+        {26, 27, 5},
+        {27, 28, 8}
+        };
+
+
+        static void Main(string[] args)
+        {
+            int intval;
+            Processing proc = new Processing(StationNames, StationsRelations);
+
+            Console.WriteLine("Список всех станций:");
+            foreach (Tuple<int, int, string> name in StationNames.OrderBy((t)=>t.Item3))
+            {
+                Console.WriteLine(name.Item3 + "-" + name.Item1);
+            }
+
+            Console.WriteLine("Введите номер начальной станции(1-28):");
+            var startStationId = Console.ReadLine();
+            Console.WriteLine("Введите номер конечной станции(1-28):");
+            var endStationId = Console.ReadLine();
+            //валидация
+            while (!Int32.TryParse(startStationId, out intval) || !Int32.TryParse(endStationId, out intval)
+                || Convert.ToInt32(startStationId) < 1 || Convert.ToInt32(startStationId) > 28
+                || Convert.ToInt32(endStationId) < 1 || Convert.ToInt32(endStationId) > 28
+                || startStationId == endStationId)
+            {
+                Console.WriteLine("Номер некорректен, попробуйте еще раз.");
+                Console.WriteLine("Введите номер начальной станции(1-28):");
+                startStationId = Console.ReadLine();
+                Console.WriteLine("Введите номер конечной станции(1-28):");
+                endStationId = Console.ReadLine();
+            }
+            
+
+            Console.WriteLine("Кратчайшее время будет(мин): ");
+            Console.WriteLine(proc.WeightMatrix[Convert.ToInt32(startStationId) - 1, Convert.ToInt32(endStationId) - 1].ToString());
+            Console.WriteLine("Кратчайший маршрут будет: ");
+            proc.StationIds.Add(Convert.ToInt32(startStationId));
+            proc.GetStationIds(Convert.ToInt32(startStationId), Convert.ToInt32(endStationId));
+            proc.StationIds.Add(Convert.ToInt32(endStationId));
+            foreach (int stationId in proc.StationIds)
+            {
+                var station = StationNames.FirstOrDefault(t => t.Item1 == stationId);
+                if (station != null)
+                    Console.WriteLine(station.Item3);
+            }
+            Console.WriteLine("Стоимость билета будет составлять(руб): " + proc.GetCost(proc.StationIds));
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
